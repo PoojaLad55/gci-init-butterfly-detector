3e83cb880f2a37c73ca20f7cd5fe267c9167aabb
Ronaldo Carrazco <racarrazco@gmail.com>
2025-04-15 11:02:05 -0400

    optical_flow update

--- methods.py
+++ methods.py
@@ -1,7 +1,7 @@
 import os
 from time import strftime
 from uuid import uuid4
-from cv2 import contourArea, boundingRect, rectangle, imwrite, resize
+from cv2 import contourArea, boundingRect, rectangle, circle, imwrite, resize
 
 def compute_iou(bbox1, bbox2):
     x1, y1, w1, h1 = bbox1
@@ -23,10 +23,10 @@ def compute_iou(bbox1, bbox2):
     iou = intersection_area / union_area
     return iou
 
-def bounding_box(contours):
+def bounding_box(contours, lower_contour_area_limit = 300):
     bounding_boxes = []
     for contour in contours:
-        if contourArea(contour) > 300:  
+        if contourArea(contour) > lower_contour_area_limit:  
             x, y, w, h = boundingRect(contour)
             bounding_boxes.append((x, y, w, h))
 
@@ -49,19 +49,39 @@ def image_dim(image, maxWidth = 56, maxHeight = 63):
     if image.shape[0] > maxWidth and image.shape[1] > maxHeight:
         return True
 
-def bbox(frame, filtered_bboxes, output_dir = "cropped_images", frame_count = 0):
-    og_frame = frame
+def bbox(frame, filtered_bboxes, og_image, output_dir = "cropped_images"):
+    og_frame = og_image
     cropped_image = frame
     for i, bbox in enumerate(filtered_bboxes):
         x, y, w, h = bbox
+        x,y,w,h = crop_og(x,y,w,h, frame, og_image_shape=og_image.shape)
         cropped_image = og_frame[y:y+h, x:x+w]
         cropped_path = os.path.join("/".join(file_name(output_dir,".png")))
-        if image_dim(cropped_image):
+        # if image_dim(cropped_image):
             
-            imwrite(cropped_path, cropped_image)
-            #rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # imwrite(cropped_path, cropped_image)
+        rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        imwrite(cropped_path, cropped_image)
+        
+        rectangle(og_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        circle(og_image, box_center_point(x,y,w,h), 10, (255,0,0),4)
     
     return frame, cropped_image
+
+def crop_og(x ,y ,w ,h ,current_image, og_image_shape = (480,640)):
+    ix, iy, _ = current_image.shape
+    print(current_image.shape)
+    new_coord = lambda a, i, s: (a / i) * s
+    nx = int(new_coord(x, ix, og_image_shape[1]))
+    ny = int(new_coord(y, iy, og_image_shape[0]))
+    nw = int(new_coord(w, ix, og_image_shape[1]))
+    nh = int(new_coord(h, iy, og_image_shape[0]))
+    return nx, ny, nw, nh
+
+
+def box_center_point(x,y,w,h):
+    return (x + int(w / 2), y + int(h / 2))
 
 def resize_image(image, dim = 240):
     return resize(image, (dim, dim))
--- optical_flow.py
+++ optical_flow.py
@@ -5,10 +5,13 @@ import time
 from methods import bounding_box, bbox, resize_image
 import sys
 
-frame_size = 240
+frame_size = 140
 
-cap = cv2.VideoCapture(0)  # Use 0 for webcam, or replace with video file path
-
+cap = cv2.VideoCapture("videos/yes04-12-2025_13_29_33-6abab022-5f22.avi")  # Use 0 for webcam, or replace with video file path
+#AMAZing04-12-2025_12_46_30-85a3a75a-2ec6.avi
+#yes04-12-2025_13_29_33-6abab022-5f22.avi
+#yes04-12-2025_14_05_01-ad6202ee-c4c1.avi
+#perfect04-14-2025_12_34_01-5141fc2a-4c45.avi
 
 max_time = 50
 
@@ -28,39 +31,47 @@ if not ret:
 
 prev_frame = resize_image(prev_frame, dim = frame_size)
 prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-
+# hsv = np.zeros_like(prev_gray)
+# hsv[..., 1] = 255
 while True:
-    ret, frame = cap.read()
+    ret, og_frame = cap.read()
     if not ret:
         break
 
-    frame = resize_image(frame, dim = frame_size)
+    frame = resize_image(og_frame, dim = frame_size)
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
     magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    # hsv[..., 0] = angle*180/np.pi/2
+    # hsv[..., 2] = cv2.normqalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+    # bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-    movement_threshold = 2.0
+    movement_threshold = 1
     movement_mask = magnitude > movement_threshold
+    
 
     contours, _ = cv2.findContours(movement_mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    filtered_bboxes = bounding_box(contours)
+    filtered_bboxes = bounding_box(contours, lower_contour_area_limit = 100)
 
     cropped_image = frame
-    frame, cropped_image = bbox(frame, filtered_bboxes, output_dir=f"{sys.argv[1]}/bbox")
+    frame, cropped_image = bbox(frame, filtered_bboxes, og_frame)#, output_dir=f'{sys.argv[1]}/images')
 
     # Display the frame with bounding boxes
-    # cv2.imshow("Movement Detection", frame)
-    sleep(0.1)
+    cv2.imshow("Movement Detection", frame)
+    cv2.imshow("og_frame", og_frame)
+    cv2.imshow("cropped", cropped_image)
+    cv2.imshow("opticalflow", movement_mask.astype(np.uint8)* 255)
+    # sleep(0.1)
 
     # Update the previous frame for the next iteration
     prev_gray = gray
 
     current_time = time.time() - start_time
 
-    if current_time >= max_time:
-        print("OP timer up")
-        break
+    # if current_time >= max_time:
+    #     print("OP timer up")
+    #     break
 
     # Break the loop if the user presses the "q" key
     if cv2.waitKey(1) & 0xFF == ord('q'):
